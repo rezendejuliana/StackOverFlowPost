b856fca920399c30901854a0876ba3cc3f9e0ea2
Juliana <juliana@rarolabs.com.br>
2021-05-06 22:09:49 -0300

    StackOverFlow Simulate Post

---
+++ StackOverFlowPost/Post.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackOverFlowPost
+{
+    partial class Program
+    {
+        public class Post
+        {
+            private string _title { get; set; }
+            private string _description { get; set; }
+            private DateTime _dateTime;
+            private readonly List<int> _countVotes = new List<int>();
+
+
+            public Post()
+            {
+                _dateTime = DateTime.Now;
+            }
+
+            public Post(string description, string title)
+            {
+                _title = title;
+                _description = description;
+                _dateTime = DateTime.Now;
+            }
+
+            public void UpVote()
+            {
+                _countVotes.Add(1);
+            }
+
+            public void DownVote()
+            {
+                _countVotes.Remove(1);
+
+            }
+
+            public int SeeVotes()
+            {
+                return _countVotes.Count;
+            }
+
+            public string SeePost()
+            {
+                string post = _title + "    " + _description;
+                return post;
+            }
+
+        }
+    }
+}
---
+++ StackOverFlowPost/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StackOverFlowPost
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "My new Post about life! S2";
+            string description = "This post is about life and stuf...";
+            Post post = new Post(description, title);
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+           // post.DownVote();
+            Console.WriteLine(post.SeePost());
+            Console.WriteLine(post.SeeVotes());
+
+
+
+        }
+    }
+}
